ace1973128e33945f4905961e05255c6e50fa8ed
Alina <alina09091999@yandex.ru>
2024-02-16 11:11:56 +0300

    first commit

---
+++ src/main/java/ru/netology/web/page/VerificationPage.java
@@ -0,0 +1,18 @@
+package ru.netology.web.page;
+
+import ru.netology.web.data.DataHelper;
+
+public class VerificationPage {
+    private final SelenideElement codeField = $("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $("[data-test-id=action-verify]");
+
+    public VerificationPage() {
+        codeField.shouldBe(visible);
+    }
+
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.setValue(verificationCode.getCode());
+        verifyButton.click();
+        return new DashboardPage();
+    }
+}
